36f227aff68e0df57999a4c52234613ffdbc620d
unknown <hassandahab11@gmail.com>
2021-01-13 13:58:28 +0100

    exercice30

---
+++ exercice30/js/main.js
@@ -0,0 +1,13 @@
+
+$(document).ready(function () {
+    $("input").keyup(function () {
+        if($("input").val().length<5){
+            $("input").addClass("is-invalid");
+        }else{
+            $("input").removeClass("is-invalid");
+            $("input").addClass("is-valid");
+
+
+        }
+    });
+}); 
